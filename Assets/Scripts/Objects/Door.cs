a51ce2564ce08fc1137b35d3db6efac0566b4e68
Henry Hu <huhenry50@gmail.com>
2025-11-16 17:59:44 -0500

    add simple interactable door with prefab

---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour, IInterableObject
+{
+    [Header("Model")]
+    [SerializeField] private Collider mainCollider;
+    private Animator anim;
+
+    [Header("Audio")]
+    private AudioSource aud;
+    [SerializeField] private AudioClip openAudio;
+    [SerializeField] private AudioClip closeAudio;
+
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        aud = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void OpenDoor()
+    {
+        mainCollider.enabled = false;
+
+        anim.SetBool("open", true);
+
+        if (aud != null)
+            aud.clip = openAudio;
+            aud.Play();
+    }
+
+    private void CloseDoor()
+    {
+        mainCollider.enabled = true;
+
+        anim.SetBool("open", false);
+
+        if (aud != null)
+            aud.clip = closeAudio;
+            aud.Play();
+    }
+
+    public void OnInteractHover()
+    {
+    }
+
+    public void OnInteractExit()
+    {
+    }
+
+    public void OnInteract()
+    {
+        OpenDoor();
+    }
+
+    public void Highlight(bool isOn)
+    {
+    }
+}
